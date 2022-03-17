db42559e4476a7f1f95c79eb0e0672fd25ef17b4
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2022-03-17 14:54:11 +0530

    Final version

--- screen3a.js
+++ screen3a.js
@@ -1,72 +1,53 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
-        numb: 1,
-        question: "The correct surgical procedure as a contraceptive method is:",
-        answer: "Vasectomy",
-        options: [
-            "Ovariectomy",
-            "Hysterectomy",
-            "Vasectomy",
-            "Castration"
-        ]
-    },
-    {
-        numb: 2,
-        question: "What is the full form of STD",
-        answer: "Sexually Transmitted Disease",
-        options: [
-            "Sexually Transmitted Disease",
-            "Single Traversal Disease",
-            "Sexually Treated Duct",
-            "Severly Treated Disease"
-        ]
-    },
-    {
-        numb: 3,
-        question: "Which of the following hormones are involved in the development of the secondary sex characteristics in males?",
-        answer: "testosterone",
-        options: [
-            "estrogen",
-            "serotonin",
-            "testosterone",
-            "follicle stimulating"
-        ]
-    },
-    {
-        numb: 4,
-        question: "Egg are formed in the",
-        answer: "Ovary",
-        options: [
-            "Fallopian tube",
-            "Uterus",
-            "Ovary",
-            "Embryo"
-        ]
-    },
-    {
-        numb: 5,
-        question: "Which of the following is a symptom of AIDS?",
-        answer: "All of the above",
-        options: [
-            "Fever",
-            "Swollen lymph nodes",
-            "Tiredness",
-            "All of the above"
-        ]
-    },
-    // you can uncomment the below codes and make duplicate as more as you want to add question
-    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  {
+    numb: 1,
+    question: "The correct surgical procedure as a contraceptive method is:",
+    answer: "Vasectomy",
+    options: ["Ovariectomy", "Hysterectomy", "Vasectomy", "Castration"],
+  },
+  {
+    numb: 2,
+    question: "What is the full form of STD",
+    answer: "Sexually Transmitted Disease",
+    options: [
+      "Sexually Transmitted Disease",
+      "Single Traversal Disease",
+      "Sexually Treated Duct",
+      "Severly Treated Disease",
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      "Which of the following hormones are involved in the development of the secondary sex characteristics in males?",
+    answer: "testosterone",
+    options: ["estrogen", "serotonin", "testosterone", "follicle stimulating"],
+  },
+  {
+    numb: 4,
+    question: "Egg are formed in the",
+    answer: "Ovary",
+    options: ["Fallopian tube", "Uterus", "Ovary", "Embryo"],
+  },
+  {
+    numb: 5,
+    question: "Which of the following is a symptom of AIDS?",
+    answer: "All of the above",
+    options: ["Fever", "Swollen lymph nodes", "Tiredness", "All of the above"],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-    //   {
-    //   numb: 6,
-    //   question: "Your Question is Here",
-    //   answer: "Correct answer of the question is here",
-    //   options: [
-    //     "Option 1",
-    //     "option 2",
-    //     "option 3",
-    //     "option 4"
-    //   ]
-    // },
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
 ];
